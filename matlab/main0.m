826227e789dd8126a76bfc4808c50a1bbeb271b1
Your Name <you@example.com>
2023-10-08 17:25:04 +0800

    2023/10/8

--- matlab/main0.m
+++ matlab/main0.m
@@ -1,18 +1,28 @@
-filename='dw8192.mat';
-%fields_dw8192=fieldnames(message_dw8192);
-%disp(fields_dw8192);
-%message_dw8192.Problem
+file=cell(8,1);
+file{1}='dw8192.mat';
+file{2}='conf5_0-4x4-10.mat';%fushu
+file{3}='epb1.mat';
+file{4}='psmigr_2.mat';
+file{5}='raefsky1.mat';
+file{6}='scircuit.mat';%太大
+file{7}='t2d_q9.mat';
+file{8}='torso2.mat';%太大
+compressibility=[];
+filename=file{6};
+compressibility(1)=top(filename)
+function [compressibility]=top(filename)
+    [shape,matrix]=get_matrix(filename);
+    disp('matrix(1,:)');
+    disp(matrix(1,:));
+    disp('shape');
+    disp(shape);
+    ma=max(matrix(:))
+    mi=min(matrix(:))
+    compressibility=hfrlc(shape,matrix);
+    %disp(compressibility);
+end
 
-[shape,matrix]=get_matrix(filename);
-disp('matrix(1,:)');
-disp(matrix(1,:));
-disp('shape');
-disp(shape);
-matrix=full(matrix);
-max=max(matrix(:))
-min=min(matrix(:))
-compressibility=hfrlc(shape,matrix);
-%disp(compressibility);
+
 
 function [shape,data]=get_matrix(filename)
     % input is sparse matrix 's filename 
@@ -46,21 +56,27 @@ function [compressibility]=hfrlc(shape,sparse_matrix)
     % 创建一个动态的cell数组，包含不同长度的字符串
     high=shape(1);
     weight=shape(2);
+    newcode_len=[];
     vector = [];
-    vector_int=cell(high,1);
-    vector_hfrlc=cell(high, 1);
+    %vector_int=cell(high,1);
+    %vector_hfrlc=cell(high, 1);
     h = waitbar(0,'压缩中，请稍等...');
     for i=1:high
-        waitbar(i/high,h,sprintf('进度：%f%%',i/high));
+        waitbar(i/high,h,sprintf('进度：%f%%',i*100/high));
         vector(i,:)=sparse_matrix(i,:);
         quantify(vector(i,:));
         vector_int{i}=quantify(vector(i,:));
-        
-       
-        vector_hfrlc{i}=yasuo(vector_int{i});
+        temp_vector_int=[];
+        temp_vector_int=vector_int{i};
+        temp_vector_int=full(temp_vector_int);
+        vector_hfrlc{i}=yasuo(temp_vector_int);
+        tempshape=size(vector_hfrlc{i});
+        newcode_len(i)=tempshape(2);
     end
     close(h);
-    compressibility=prod(size(vector_hfrlc))*1.0/(prod(shape)*4)
+    sum(newcode_len)
+    prod(shape)
+    compressibility=sum(newcode_len)*1.0/(prod(shape));
     
 end
 
