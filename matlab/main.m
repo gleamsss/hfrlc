ad388a488c5ada013feb41ac172b10c26aded10b
Your Name <you@example.com>
2023-10-09 17:50:33 +0800

    abc

--- matlab/main.m
+++
@@ -1,8 +0,0 @@
-[shape,matrix]=get_matrix('dw8192.mat')
-function [shape,data]=get_matrix(filename)
-    % input is sparse matrix 's filename 
-    % output is matrix shape and data
-    message=load(filename);
-    data=message.Problem.A;
-    shape=size(data);
-end
